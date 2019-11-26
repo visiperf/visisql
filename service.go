e1cf663adda74ae59e5a30b1f19e60ba12c45068
Romain Jeannoutot <romain.jeannoutot@visiperf.com>
2019-11-26 11:12:09 +0100

    set predicates slice of slice on get method

--- service.go
+++ service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/huandu/go-sqlbuilder"
 	"reflect"
+	"strings"
 )
 
 type SqlService struct {
@@ -95,7 +96,7 @@ func (ss *SqlService) List(identifier string, fields []string, from string, join
 	return nil
 }
 
-func (ss *SqlService) Get(identifier string, fields []string, from string, joins []*Join, predicates []*Predicate, v interface{}) error {
+func (ss *SqlService) Get(identifier string, fields []string, from string, joins []*Join, predicates [][]*Predicate, v interface{}) error {
 	builder := sqlbuilder.PostgreSQL.NewSelectBuilder()
 
 	builder.Select(fields...)
@@ -109,22 +110,29 @@ func (ss *SqlService) Get(identifier string, fields []string, from string, joins
 		}
 	}
 
-	for _, p := range predicates {
-		if p.IsOperator(OperatorIn) {
-			builder.Where(builder.In(p.Field, p.Values...))
-		}
-		if p.IsOperator(OperatorEqual) {
-			if len(p.Values) != 1 {
-				return fmt.Errorf(`predicate must have only one value when operator is equal`)
+	for _, pAnd := range predicates {
+		var orExprs []string
+		for _, pOr := range pAnd {
+			if pOr.IsOperator(OperatorIn) {
+				//builder.Where(builder.In(p.Field, p.Values...))
+				orExprs = append(orExprs, builder.In(pOr.Field, pOr.Values...))
 			}
-			builder.Where(builder.Equal(p.Field, p.Values[0]))
-		}
-		if p.IsOperator(OperatorLike) {
-			if len(p.Values) != 1 {
-				return fmt.Errorf(`predicate must have only one value when operator is like`)
+			if pOr.IsOperator(OperatorEqual) {
+				if len(pOr.Values) != 1 {
+					return fmt.Errorf(`predicate must have only one value when operator is equal`)
+				}
+				//builder.Where(builder.Equal(p.Field, p.Values[0]))
+				orExprs = append(orExprs, builder.Equal(pOr.Field, pOr.Values[0]))
 			}
-			builder.Where(builder.Like(p.Field, p.Values[0]))
+			if pOr.IsOperator(OperatorLike) {
+				if len(pOr.Values) != 1 {
+					return fmt.Errorf(`predicate must have only one value when operator is like`)
+				}
+				//builder.Where(builder.Like(p.Field, p.Values[0]))
+				orExprs = append(orExprs, builder.Like(pOr.Field, pOr.Values[0]))
+			}
 		}
+		builder.Where(fmt.Sprintf("( %s )", strings.Join(orExprs, " OR ")))
 	}
 
 	builder.GroupBy(identifier)
