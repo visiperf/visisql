160ecba3d9d41d84a5af4af5a06dc9d83cd42625
Romain Jeannoutot <romain@visiperf.io>
2020-01-16 14:18:05 +0100

    implement insert multiple method on transaction service

--- transaction.go
+++ transaction.go
@@ -2,7 +2,6 @@ package visisql
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	"github.com/huandu/go-sqlbuilder"
 )
@@ -44,9 +43,49 @@ func (ts *TransactionService) Insert(into string, values map[string]interface{},
 	return resp, nil
 }
 
-// @todo: implement TransactionService.InsertMultiple()
 func (ts *TransactionService) InsertMultiple(into string, fields []string, values [][]interface{}, returning interface{}) ([]interface{}, error) {
-	return nil, errors.New("not implemented")
+	builder := sqlbuilder.PostgreSQL.NewInsertBuilder()
+
+	builder.InsertInto(into)
+
+	builder.Cols(fields...)
+	builder.Values(values[0]...)
+
+	query, _ := builder.Build()
+	if returning != nil {
+		query = fmt.Sprintf("%s returning %s", query, returning)
+	}
+
+	stmt, err := ts.tx.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	var resps []interface{}
+	for _, args := range values {
+		if returning != nil {
+			var resp interface{}
+
+			row := stmt.QueryRow(args...)
+			if err := row.Scan(&resp); err != nil {
+				if rErr := ts.tx.Rollback(); rErr != nil {
+					return nil, rErr
+				}
+				return nil, err
+			}
+
+			resps = append(resps, resp)
+		} else {
+			if _, err := stmt.Exec(args...); err != nil {
+				if rErr := ts.tx.Rollback(); rErr != nil {
+					return nil, rErr
+				}
+				return nil, err
+			}
+		}
+	}
+
+	return resps, nil
 }
 
 func (ts *TransactionService) Update(table string, set map[string]interface{}, predicates [][]*Predicate) error {
