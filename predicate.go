e8aa07359e7748fe55b0f978d6f39c592ff3c268
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2020-06-18 10:52:50 +0200

    return wrapped query errors in predicates to string function

--- predicate.go
+++ predicate.go
@@ -1,10 +1,18 @@
 package visisql
 
 import (
+	"errors"
 	"fmt"
-	"github.com/huandu/go-sqlbuilder"
 	"strings"
+
+	"github.com/huandu/go-sqlbuilder"
 )
+
+var errOperatorEqual = errors.New("predicate must have only one value when operator is equal")
+var errOperatorLike = errors.New("predicate must have only one value when operator is like")
+var errOperatorIsNull = errors.New("predicate should not have value(s) when operator is null")
+var errOperatorLessThan = errors.New("predicate must have only one value when operator is less than")
+var errOperatorGreaterThan = errors.New("predicate must have only one value when operator is greater than")
 
 type Operator string
 
@@ -42,31 +50,31 @@ func predicatesToStrings(predicates [][]*Predicate, cond *sqlbuilder.Cond) ([]st
 			}
 			if pOr.IsOperator(OperatorEqual) {
 				if len(pOr.Values) != 1 {
-					return nil, fmt.Errorf(`predicate must have only one value when operator is equal`)
+					return nil, fmt.Errorf("visisql predicates: %w", &QueryError{errOperatorEqual})
 				}
 				orExprs = append(orExprs, cond.Equal(pOr.Field, pOr.Values[0]))
 			}
 			if pOr.IsOperator(OperatorLike) {
 				if len(pOr.Values) != 1 {
-					return nil, fmt.Errorf(`predicate must have only one value when operator is like`)
+					return nil, fmt.Errorf("visisql predicates: %w", &QueryError{errOperatorLike})
 				}
 				orExprs = append(orExprs, cond.Like(pOr.Field, pOr.Values[0]))
 			}
 			if pOr.IsOperator(OperatorIsNull) {
 				if len(pOr.Values) > 0 {
-					return nil, fmt.Errorf(`predicate should not have value(s) when operator is null`)
+					return nil, fmt.Errorf("visisql predicates: %w", &QueryError{errOperatorIsNull})
 				}
 				orExprs = append(orExprs, cond.IsNull(pOr.Field))
 			}
 			if pOr.IsOperator(OperatorLessThan) {
 				if len(pOr.Values) != 1 {
-					return nil, fmt.Errorf(`predicate must have only one value when operator is less than`)
+					return nil, fmt.Errorf("visisql predicates: %w", &QueryError{errOperatorLessThan})
 				}
 				orExprs = append(orExprs, cond.LessThan(pOr.Field, pOr.Values[0]))
 			}
 			if pOr.IsOperator(OperatorGreaterThan) {
 				if len(pOr.Values) != 1 {
-					return nil, fmt.Errorf(`predicate must have only one value when operator is greater than`)
+					return nil, fmt.Errorf("visisql predicates: %w", &QueryError{errOperatorGreaterThan})
 				}
 				orExprs = append(orExprs, cond.GreaterThan(pOr.Field, pOr.Values[0]))
 			}
