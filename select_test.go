331708c4ca3aad501453ac7303111ff19fd0af36
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2020-07-21 16:41:11 +0200

    implement test for build method

--- select_test.go
+++ select_test.go
@@ -3,11 +3,13 @@ package visisql
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
 )
 
 type User struct {
@@ -65,7 +67,82 @@ func wrapper(schema Schema, t *testing.T, test func(db *sqlx.DB)) {
 }
 
 func TestBuild(t *testing.T) {
+	type BuildReq struct {
+		fields     []string
+		from       string
+		joins      []*Join
+		predicates [][]*Predicate
+		groupBy    []string
+		orderBy    []*OrderBy
+		pagination *Pagination
+	}
 
+	type BuildResp struct {
+		query string
+		args  []interface{}
+		err   error
+	}
+
+	var tests = []struct {
+		Req     *BuildReq
+		Resp    *BuildResp
+		Message string
+	}{{
+		Message: "bad predicates",
+		Req: &BuildReq{
+			fields: []string{"id", "email", "roles", "created_at"},
+			from:   "user",
+			joins:  nil,
+			predicates: [][]*Predicate{{
+				NewPredicate("id", OperatorEqual, []interface{}{1, 2}),
+			}},
+			groupBy:    nil,
+			orderBy:    nil,
+			pagination: nil,
+		},
+		Resp: &BuildResp{
+			query: "",
+			args:  nil,
+			err:   &QueryError{errOperatorEqual},
+		},
+	}, {
+		Message: "ok",
+		Req: &BuildReq{
+			fields: []string{"id", "email", "roles", "created_at"},
+			from:   "user",
+			joins: []*Join{
+				NewJoin(LeftJoin, "company", "user.company_id = company.id"),
+			},
+			predicates: [][]*Predicate{{
+				NewPredicate("user.id", OperatorEqual, []interface{}{1}),
+			}},
+			groupBy: []string{"user.id"},
+			orderBy: []*OrderBy{
+				NewOrderBy("user.id", OrderAsc),
+			},
+			pagination: NewPagination(1, 2),
+		},
+		Resp: &BuildResp{
+			query: `SELECT id, email, roles, created_at FROM user LEFT JOIN company ON user.company_id = company.id WHERE ( user.id = $1 ) GROUP BY user.id ORDER BY user.id ASC LIMIT 2 OFFSET 1`,
+			args:  []interface{}{1},
+			err:   nil,
+		},
+	}}
+
+	ss := NewSelectService(nil)
+
+	for _, test := range tests {
+		query, args, err := ss.Build(test.Req.fields, test.Req.from, test.Req.joins, test.Req.predicates, test.Req.groupBy, test.Req.orderBy, test.Req.pagination)
+
+		assert.Equal(t, test.Resp.query, query, test.Message)
+		assert.Equal(t, test.Resp.args, args, test.Message)
+
+		if test.Resp.err != nil {
+			assert.True(t, strings.Contains(err.Error(), test.Resp.err.Error()), test.Message)
+		} else {
+			assert.Nil(t, err, test.Message)
+		}
+	}
 }
 
 func TestQuery(t *testing.T) {
